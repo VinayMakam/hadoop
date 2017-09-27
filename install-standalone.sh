5c6bac48caf45824e5bae5ccc8c18be011946811
Vinay Makam <vinay.makam@gmail.com>
2017-09-27 15:44:33 +0000

    Update install-standalone.sh

--- install-standalone.sh
+++ install-standalone.sh
@@ -2,7 +2,7 @@
 
 : "
   Copyright (C) 2011-:
-        Vinay Makam, vinay.makam@yahoo.com
+        Vinay Makam, www.linkedin.com/in/vinaymakam
 
   Permission is hereby granted, free of charge, to any person obtaining a
   copy of this software and associated documentation files (the "Software"), to
